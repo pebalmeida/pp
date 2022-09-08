a4c4e21ca228603ef37013e227a3f9938412c6b4
pebalmeida <93488924+pebalmeida@users.noreply.github.com>
2022-09-08 16:06:44 -0300

    pp

---
+++ text.java
@@ -0,0 +1,11 @@
+ import java.io.*;
+ 
+ class text {
+    public static void main(String args[]){
+        String TEXTO1 = "Hello";
+        String TEXTO2 = "World";
+        System.out.println(TEXTO1.concat(TEXTO2));
+        System.out.println(TEXTO1 + TEXTO2);
+    }
+    
+}
